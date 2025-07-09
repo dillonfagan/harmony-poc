bb19110b99edced5d20c7cef9ab38476702c7b14
Dillon Fagan <dillonfagan@pm.me>
2025-07-09 17:05:17 -0400

    chore: Add Invest card to home page.

--- apps/consumer/app/(dashboard)/home/page.tsx
+++ apps/consumer/app/(dashboard)/home/page.tsx
@@ -2,6 +2,7 @@ import { ResponsiveGrid, StretchColumn } from "@/components/layouts";
 import PageHeading from "@/components/PageHeading";
 import ConsumptionCreditsCard from "@/views/ConsumptionCreditsCard";
 import InvestmentCreditsCard from "@/views/InvestmentCreditsCard";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       </ResponsiveGrid>
       <ResponsiveGrid>
         <ActivationCard />
+        <InvestCard />
       </ResponsiveGrid>
     </StretchColumn>
   );
@@ -20,10 +22,20 @@ export default function Home() {
 
 function ActivationCard() {
   return (
-    <div className="bg-base-200 card p-4 shadow-sm">
+    <div className="flex flex-col bg-base-200 card p-4 shadow-sm">
       <h2 className="text-2xl font-semibold mb-3">Harmony Card</h2>
-      <span className="mb-4">It looks like you still haven't activated your Harmony Card. Use it to redeem your consumption credits at participating merchants.</span>
+      <span className="grow mb-4">It looks like you still haven't activated your Harmony Card. Use it to redeem your consumption credits at participating merchants.</span>
       <button className="btn btn-primary">Activate Card</button>
+    </div>
+  );
+}
+
+function InvestCard() {
+  return (
+    <div className="flex flex-col bg-base-200 card p-4 shadow-sm">
+      <h2 className="text-2xl font-semibold mb-3">Invest in Community</h2>
+      <span className="grow mb-4">Use your investment credits to direct funding to community projects of your choosing. It's easy to get started.</span>
+      <Link href="/invest" className="btn btn-secondary">Start Investing</Link>
     </div>
   );
 }
--- apps/consumer/components/PageHeading.tsx
+++ apps/consumer/components/PageHeading.tsx
@@ -6,7 +6,7 @@ type Props = {
 export default function PageHeading({ title, subtitle }: Props) {
   return (
     <div className="mb-4">
-      <h1 className="text-3xl lg:text-5xl">{title}</h1>
+      <h1 className="text-4xl lg:text-5xl">{title}</h1>
       {subtitle && <span className="block text-lg mt-4">{subtitle}</span>}
     </div>
   );
